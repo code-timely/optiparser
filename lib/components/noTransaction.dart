d303f9190354c210163238bed7afbe31cc6d93c2
piyushsingal3 <157308982+piyushsingal3@users.noreply.github.com>
2024-09-05 06:56:29 +0530

    Create noTransaction.dart

---
+++ lib/components/noTransaction.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class Notransaction extends StatelessWidget {
+
+  const Notransaction({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white, // Choose a suitable background color
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(5.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Lottie animation for processing
+              Lottie.asset(
+                'assets/Home.json',
+                width: 180,
+                height: 180,
+              ),
+              // Space between animation and text
+              // Descriptive text
+              Text(
+                'No Transactions!',
+                style: TextStyle(
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey[700],
+                ),
+                textAlign: TextAlign.center,
+              ),
+              // Space between text and progress message
+              // Progress message or tagline
+              Text(
+                'Update your first transaction by clicking the button below',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey[500],
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
